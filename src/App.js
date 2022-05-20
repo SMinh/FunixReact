3e5319bdf4b3be6b25652b3287879a001b841084
Son Minh <minh.huynhcao@gmail.com>
2022-05-20 13:16:25 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -13,6 +14,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
