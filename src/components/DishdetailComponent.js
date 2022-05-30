736e0523c33dddd5bca8df2c751bfa38677357eb
Son Minh <minh.huynhcao@gmail.com>
2022-05-30 22:01:20 +0700

    lab11

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -12,6 +12,7 @@ import { Link } from "react-router-dom";
 import CommentForm from "./CommentForm";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 const DishDetail = (props) => {
   const [isCommentForm, setIsCommentForm] = useState(false);
@@ -23,12 +24,19 @@ const DishDetail = (props) => {
   function RenderDish({ dish }) {
     if (dish != null) {
       return (
-        <Card>
-          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-          <CardImgOverlay>
-            <CardTitle>{dish.name}</CardTitle>
-          </CardImgOverlay>
-        </Card>
+        <FadeTransform
+          in
+          transformProps={{
+            exitTransform: "scale(0.5) translateY(-50%)",
+          }}
+        >
+          <Card>
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </FadeTransform>
       );
     } else {
       return <div></div>;
@@ -41,21 +49,25 @@ const DishDetail = (props) => {
         <div>
           <h4>Comments</h4>
           <ul className="list-unstyled">
-            {comments.map((comment) => {
-              return (
-                <li key={comment.id}>
-                  <p>{comment.comment}</p>
-                  <p>
-                    --{comment.author},{" "}
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "2-digit",
-                    }).format(new Date(Date.parse(comment.date)))}
-                  </p>
-                </li>
-              );
-            })}
+            <Stagger in>
+              {comments.map((comment) => {
+                return (
+                  <Fade in>
+                    <li key={comment.id}>
+                      <p>{comment.comment}</p>
+                      <p>
+                        --{comment.author},{" "}
+                        {new Intl.DateTimeFormat("en-US", {
+                          year: "numeric",
+                          month: "short",
+                          day: "2-digit",
+                        }).format(new Date(Date.parse(comment.date)))}
+                      </p>
+                    </li>
+                  </Fade>
+                );
+              })}
+            </Stagger>
           </ul>
           <Button color="primary" onClick={toggleModalComment}>
             <span className="fa fa-edit fa-lg"></span> Submit
